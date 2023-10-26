c429111657e963e8999c372bb06c0696c1f2b2d1
Peter Buchegger <peter.buchegger7@gmail.com>
2023-10-26 12:45:31 +0000

    update

--- src/MqttLogger.h
+++ src/MqttLogger.h
@@ -1,51 +1,35 @@
-/*
-  MqttLogger - offer print() interface like Serial but by publishing to a given mqtt topic. 
-               Uses Serial as a fallback when no mqtt connection is available.
-
-  Claus Denk
-  https://androbi.com
-*/
-
-#ifndef MqttLogger_h
-#define MqttLogger_h
+#ifndef MQTTLOGGER_H_
+#define MQTTLOGGER_H_
 
 #include <Arduino.h>
 #include <Print.h>
 #include <PubSubClient.h>
+#include <vector>
 
 enum MqttLoggerMode {
-    MqttAndSerialFallback = 0,
-    SerialOnly = 1,
-    MqttOnly = 2,
-    MqttAndSerial = 3,
+  MqttAndSerialFallback,
+  SerialOnly,
+  MqttOnly,
+  MqttAndSerial
 };
 
-class MqttLogger : public Print
-{
-private:
-    const char* topic;
-    uint8_t* buffer;
-    uint8_t* bufferEnd;
-    uint16_t bufferCnt = 0, bufferSize = 0;
-    PubSubClient* client;
-    MqttLoggerMode mode;
-    void sendBuffer();
-
+class MqttLogger : public Print {
 public:
-    MqttLogger(MqttLoggerMode mode=MqttLoggerMode::MqttAndSerialFallback);
-    MqttLogger(PubSubClient& client, const char* topic, MqttLoggerMode mode=MqttLoggerMode::MqttAndSerialFallback);
-    ~MqttLogger();
+  MqttLogger(PubSubClient &client, const char *topic, const MqttLoggerMode mode = MqttLoggerMode::MqttAndSerialFallback);
+  ~MqttLogger();
 
-    void setClient(PubSubClient& client);
-    void setTopic(const char* topic);
-    void setMode(MqttLoggerMode mode);
-    void setRetained(boolean retained);
-    
-    virtual size_t write(uint8_t);
-    using Print::write;
-    
-    uint16_t getBufferSize();
-    boolean setBufferSize(uint16_t size);
+  virtual size_t write(uint8_t character);
+  using Print::write;
+
+private:
+  const MqttLoggerMode _mode;
+  const char          *_topic;
+
+  PubSubClient &_client;
+
+  std::vector<uint8_t> _buffer;
+
+  void _sendBuffer();
 };
 
 #endif
