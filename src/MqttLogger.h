c991e1ef387c8ce05233aed143157c6240dbc044
Peter Buchegger <peterus@users.noreply.github.com>
2024-01-13 09:46:32 +0100

    Update MqttLogger.h

--- src/MqttLogger.h
+++ src/MqttLogger.h
@@ -1,10 +1,13 @@
 #ifndef MQTTLOGGER_H_
 #define MQTTLOGGER_H_
 
+#include <array>
+
 #include <Arduino.h>
 #include <Print.h>
 #include <PubSubClient.h>
-#include <vector>
+
+constexpr unsigned int MQTT_LOGGER_MAX_ELEMENTS = 255;
 
 enum MqttLoggerMode {
   MqttAndSerialFallback,
@@ -15,19 +18,21 @@ enum MqttLoggerMode {
 
 class MqttLogger : public Print {
 public:
-  MqttLogger(PubSubClient &client, const char *topic, const MqttLoggerMode mode = MqttLoggerMode::MqttAndSerialFallback);
+  MqttLogger(PubSubClient &client, String topic, const MqttLoggerMode mode = MqttLoggerMode::MqttAndSerialFallback);
   ~MqttLogger();
+
+  void setTopic(const String &topic);
 
   virtual size_t write(uint8_t character);
   using Print::write;
 
 private:
   const MqttLoggerMode _mode;
-  const char          *_topic;
+  String              &_topic;
+  PubSubClient        &_client;
 
-  PubSubClient &_client;
-
-  std::vector<uint8_t> _buffer;
+  std::array<uint8_t, MQTT_LOGGER_MAX_ELEMENTS>           _buffer;
+  std::array<uint8_t, MQTT_LOGGER_MAX_ELEMENTS>::iterator _buffer_iter;
 
   void _sendBuffer();
 };
