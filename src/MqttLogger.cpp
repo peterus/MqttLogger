50ede63b4a910242f8c39fce95857ecfaaa4b5bf
Peter Buchegger <peterus@users.noreply.github.com>
2024-01-13 09:46:44 +0100

    Update MqttLogger.cpp

--- src/MqttLogger.cpp
+++ src/MqttLogger.cpp
@@ -1,32 +1,42 @@
 #include "MqttLogger.h"
 
-MqttLogger::MqttLogger(PubSubClient &client, const char *topic, const MqttLoggerMode mode) : _mode(mode), _topic(topic), _client(client) {
+MqttLogger::MqttLogger(PubSubClient &client, String topic, const MqttLoggerMode mode) : _mode(mode), _topic(topic), _client(client), _buffer({0}), _buffer_iter(_buffer.begin()) {
 }
 
 MqttLogger::~MqttLogger() {
 }
 
+void MqttLogger::setTopic(const String &topic) {
+  _topic = topic;
+}
+
 size_t MqttLogger::write(uint8_t character) {
   if (character == '\n') {
     _sendBuffer();
+  } else if (_buffer_iter == _buffer.end()) {
+    _sendBuffer();
+    *_buffer_iter = character;
+    _buffer_iter++;
   } else {
-    _buffer.push_back(character);
+    *_buffer_iter = character;
+    _buffer_iter++;
   }
   return 1;
 }
 
 void MqttLogger::_sendBuffer() {
-  if (_buffer.empty()) {
+  if (_buffer_iter == _buffer.begin()) {
     return;
   }
   bool doSerial = _mode == MqttLoggerMode::SerialOnly || _mode == MqttLoggerMode::MqttAndSerial;
   if (_mode != MqttLoggerMode::SerialOnly && _client.connected()) {
-    _client.publish(_topic, reinterpret_cast<const uint8_t *>(_buffer.data()), _buffer.size());
+    _client.publish(_topic.c_str(), reinterpret_cast<const uint8_t *>(_buffer.data()), _buffer_iter - _buffer.begin());
   } else if (_mode == MqttLoggerMode::MqttAndSerialFallback) {
     doSerial = true;
   }
   if (doSerial) {
-    Serial.write(reinterpret_cast<const uint8_t *>(_buffer.data()), _buffer.size());
+    Serial.write(reinterpret_cast<const uint8_t *>(_buffer.data()), _buffer_iter - _buffer.begin());
     Serial.println();
   }
+  _buffer_iter = _buffer.begin();
 }
