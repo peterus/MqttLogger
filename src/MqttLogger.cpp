b69296c11169700ef7b657ab7e7ad491dc2d78b7
Peter Buchegger <peterus@users.noreply.github.com>
2024-01-14 20:30:01 +0100

    Update MqttLogger.cpp

--- src/MqttLogger.cpp
+++ src/MqttLogger.cpp
@@ -1,5 +1,7 @@
 #include "MqttLogger.h"
 
+#include <ArduinoJson.h>
+
 MqttLogger::MqttLogger(PubSubClient &client, String topic, const MqttLoggerMode mode) : _mode(mode), _topic(topic), _client(client), _buffer({0}), _buffer_iter(_buffer.begin()) {
 }
 
@@ -17,9 +19,13 @@ size_t MqttLogger::write(uint8_t character) {
     _sendBuffer();
     *_buffer_iter = character;
     _buffer_iter++;
+    *_buffer_iter = 0;
+  } else if (character == '\r') {
+    return 1;
   } else {
     *_buffer_iter = character;
     _buffer_iter++;
+    *_buffer_iter = 0;
   }
   return 1;
 }
@@ -29,14 +35,23 @@ void MqttLogger::_sendBuffer() {
     return;
   }
   bool doSerial = _mode == MqttLoggerMode::SerialOnly || _mode == MqttLoggerMode::MqttAndSerial;
+
+  StaticJsonDocument<MQTT_LOGGER_MAX_ELEMENTS + 50> doc;
+  doc["time"] = millis();
+  doc["msg"]  = reinterpret_cast<const uint8_t *>(_buffer.data());
+
+  char buffer[MQTT_LOGGER_MAX_ELEMENTS + 50 + 50];
+  serializeJson(doc, buffer);
+
   if (_mode != MqttLoggerMode::SerialOnly && _client.connected()) {
-    _client.publish(_topic.c_str(), reinterpret_cast<const uint8_t *>(_buffer.data()), _buffer_iter - _buffer.begin());
+    _client.publish(_topic.c_str(), buffer);
   } else if (_mode == MqttLoggerMode::MqttAndSerialFallback) {
     doSerial = true;
   }
   if (doSerial) {
-    Serial.write(reinterpret_cast<const uint8_t *>(_buffer.data()), _buffer_iter - _buffer.begin());
+    Serial.write(buffer);
     Serial.println();
   }
-  _buffer_iter = _buffer.begin();
+  _buffer_iter  = _buffer.begin();
+  *_buffer_iter = 0;
 }
